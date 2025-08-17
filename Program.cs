029c47a6d48c15d0708024fb7df2dc414b73aaac
deshanm123 <deshanm123@gmail.com>
2025-08-17 19:19:09 +0530

    completed the application to calculate amarks with extra credit

--- Program.cs
+++ Program.cs
@@ -1,56 +1,95 @@
 ﻿using System;
 // initialize variables - graded assignments 
 int currentAssignments = 5;
+string[] studentNames = { "Sophia", "Andrew", "Emma", "Logan" };
 
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
-
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
-
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
+foreach (string studentName in studentNames)
+{
+    decimal averageScore = getAverageScore(sumOfScores(getScoresOfStudent(studentName)));
+    Console.WriteLine($"{studentName}:\t\t {averageScore} \t{getAverageScoreGrade(averageScore)}");
+}
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
+
+
+int[] getScoresOfStudent(string studentName)
+{
+    if (studentName == "Sophia")
+        return sophiaScores;
+
+    else if (studentName == "Andrew")
+        return andrewScores;
+
+    else if (studentName == "Emma")
+        return emmaScores;
+
+    else if (studentName == "Logan")
+        return loganScores;
+    else
+        throw new NotImplementedException();
+}
+
+
+int sumOfScores(int[] scoreArr)
+{
+    int scoreCount = 1;
+    int sumofScores = 0;
+    foreach (int score in scoreArr)
+    {   
+        if (scoreCount <= currentAssignments)
+        {
+            sumofScores += score;
+        }
+        else
+        {
+           // sumofScores += score / 10;
+            //sumofScores  += Convert.ToInt32( score * 0.1M);
+        }
+        scoreCount++;
+    }
+    return sumofScores;
+}
+
+decimal getAverageScore(int sumOfScores)
+{
+    return (decimal)sumOfScores / currentAssignments;
+}
+
+
+string getAverageScoreGrade(decimal averageScore)
+{
+    if (averageScore >= 97.0M && averageScore <= 100.0M)
+        return "A+";
+    else if (averageScore >= 93.0M)
+        return "A";
+    else if (averageScore >= 90.0M)
+        return "A-";
+    else if (averageScore >= 87.0M)
+        return "B+";
+    else if (averageScore >= 83.0M)
+        return "B";
+    else if (averageScore >= 80.0M)
+        return "B-";
+    else if (averageScore >= 77.0M)
+        return "C+";
+    else if (averageScore >= 73.0M)
+        return "C";
+    else if (averageScore >= 70.0M)
+        return "C-";
+    else if (averageScore >= 67.0M)
+        return "D+";
+    else if (averageScore >= 63.0M)
+        return "D";
+    else if (averageScore >= 60.0M)
+        return "D-";
+    else if (averageScore <= 59.0M && averageScore >= 0.0M)
+        return "F";
+    else
+        throw new Exception("Cannot provide a grade due to Invalid Marks");
+}
